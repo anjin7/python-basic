d32986b34de65cae715c98952b1ff59610e71a54
_An_Jin_ <aji0719@gmail.com>
2022-11-08 18:20:40 +0900

    5.6

--- jobscrapper.py
+++ jobscrapper.py
@@ -8,7 +8,12 @@ response = get(f"{base_url}{search_term}")
 if response.status_code != 200:
   print("Can't request website")
 else:
+  # print(response.text)
   soup = BeautifulSoup(response.text, "html.parser")
   jobs = (soup.find_all('section', class_="jobs"))
-
-  
+  # print(len(jobs))
+  for job_section in jobs:
+    job_posts = job_section.find_all('li')
+    for post in job_posts:
+      print(post)
+      print("//////////////")
