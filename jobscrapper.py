2f6bda0207d3ed05f210015139682baeb794e02a
_An_Jin_ <aji0719@gmail.com>
2022-11-11 20:11:11 +0900

    5.8.1

--- jobscrapper.py
+++ jobscrapper.py
@@ -1,32 +1,33 @@
 from requests import get
 from bs4 import BeautifulSoup
 
-base_url = "https://weworkremotely.com/remote-jobs/serch?term="
-search_term = "python"
-
-response = get(f"{base_url}{search_term}")
-result = []
-if response.status_code != 200:
-  print("Can't request website")
-else:
-  # print(response.text)
-  soup = BeautifulSoup(response.text, "html.parser")
-  jobs = (soup.find_all('section', class_="jobs"))
-  # print(len(jobs))
-  for job_section in jobs:
-    job_posts = job_section.find_all('li')
-    job_posts.pop(-1)
-    for post in job_posts:
-      anchors = post.find_all('a')
-      anchor = anchors[1]
-      link =  anchor['href']
-      company, kind, region = anchor.find_all('span', class_="company")
-      # print(company, kind, region)
-      position = anchor.find('span', class_='title')
-      job_data = {
-        'company': company.string,
-        'region': region.string,
-        'position': position.string,
-      }
-      result.append(job_data)
-print(result)
+def extra_jobs(keyword):
+  base_url = "https://weworkremotely.com/remote-jobs/serch?term="
+  search_term = "python"
+  
+  response = get(f"{base_url}{search_term}")
+  result = []
+  if response.status_code != 200:
+    print("Can't request website")
+  else:
+    # print(response.text)
+    soup = BeautifulSoup(response.text, "html.parser")
+    jobs = (soup.find_all('section', class_="jobs"))
+    # print(len(jobs))
+    for job_section in jobs:
+      job_posts = job_section.find_all('li')
+      job_posts.pop(-1)
+      for post in job_posts:
+        anchors = post.find_all('a')
+        anchor = anchors[1]
+        # link =  anchor['href']
+        company, kind, region = anchor.find_all('span', class_="company")
+        # print(company, kind, region)
+        position = anchor.find('span', class_='title')
+        job_data = {
+          'company': company.string,
+          'region': region.string,
+          'position': position.string,
+        }
+        result.append(job_data)
+  print(result)
